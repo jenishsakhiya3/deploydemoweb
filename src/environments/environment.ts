eb888f6835e5557c3a229c6738238cc2bdce36b2
SERVER7\pca13 <jenish.sakhiya@etatvasoft.com>
2025-01-24 15:04:57 +0530

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseURL: 'http://192.168.5.13:7107/',
+  baseURL: 'https://deploydemoapi-gfbqgca9dnhedkg8.canadacentral-01.azurewebsites.net/',
 };
